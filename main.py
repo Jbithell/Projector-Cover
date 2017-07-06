7f8136bb2eb929a8780fb5451354290f85a5f734
James Bithell <hi@jbithell.com>
2017-07-06 16:25:00 +0100

    Add serial

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 
-#           BUTTONS
+#                                                               BUTTONS
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-#           STEPPER MOTOR
+#                                                               STEPPER MOTOR
 motorGpioPins = [17, 18, 27, 22] #17 = 1 etc.
 for pin in motorGpioPins:
     GPIO.setup(pin, GPIO.OUT)
@@ -20,6 +20,12 @@ motorStepSequence[4] = [motorGpioPins[2]]
 motorStepSequence[5] = [motorGpioPins[2], motorGpioPins[3]]
 motorStepSequence[6] = [motorGpioPins[3]]
 motorStepSequence[7] = [motorGpioPins[3], motorGpioPins[0]]
+
+#                                                                  LCD
+ser = serial.Serial('/dev/tty.usbserial', 9600, timeout=0.5)
+ser.write('testtesttest$$')
+time.sleep(5)
+ser.write('Hi$')
 
 def motor(anticlockwise): #Run the motor for an "instant"
     global motorGpioPins, motorStepSequence
