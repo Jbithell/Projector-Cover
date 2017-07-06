65b5702e6bad96f6dee90a8e126305c2851be1ff
James Bithell <hi@jbithell.com>
2017-07-06 19:35:04 +0100

    Try a lib

--- main.py
+++ main.py
@@ -3,65 +3,18 @@
 import sys
 import time
 import RPi.GPIO as GPIO
+import StepperMotor
 
-# Use BCM GPIO references
-# instead of physical pin numbers
-GPIO.setmode(GPIO.BCM)
 
-StepPins = [18,23,24,25]
-
-# Set all pins as output
-for pin in StepPins:
-    print
-    "Setup pins"
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, False)
-
-# Define advanced sequence
-# as shown in manufacturers datasheet
-Seq = [[1, 0, 0, 1],
-       [1, 0, 0, 0],
-       [1, 1, 0, 0],
-       [0, 1, 0, 0],
-       [0, 1, 1, 0],
-       [0, 0, 1, 0],
-       [0, 0, 1, 1],
-       [0, 0, 0, 1]]
-
-StepCount = len(Seq)
-StepDir = 2  # Set to 1 or 2 for clockwise
-# Set to -1 or -2 for anti-clockwise
-
-# Read wait time from command line
-WaitTime = 10 / float(1000)
-
-# Initialise variables
-StepCounter = 0
-
-# Start main loop
-while True:
-
-    print
-    StepCounter,
-    print
-    Seq[StepCounter]
-
-    for pin in range(0, 4):
-        xpin = StepPins[pin]  # Get GPIO
-        if Seq[StepCounter][pin] != 0:
-            print
-            " Enable GPIO %i" % (xpin)
-            GPIO.output(xpin, True)
-        else:
-            GPIO.output(xpin, False)
-
-    StepCounter += StepDir
-
-    # If we reach the end of the sequence
-    if (StepCounter >= StepCount):
-        StepCounter = 0
-    if (StepCounter < 0):
-        StepCounter = StepCount + StepDir
-
-    # Wait before moving on
-    time.sleep(WaitTime)
+GPIO.setmode(GPIO.BOARD)
+m = Motor([18,23,24,25])
+m.rpm = 5
+m.move_to(90)
+sleep(1)
+m.move_to(0)
+sleep(1)
+m.mode = 2
+m.move_to(90)
+sleep(1)
+m.move_to(0)
+GPIO.cleanup()
