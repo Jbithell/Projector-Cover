d02a1713d78c7a72dd1eca8e7b52fe053f50084d
James Bithell <hi@jbithell.com>
2017-07-06 19:21:19 +0100

    Try a different speed

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ StepDir = 2  # Set to 1 or 2 for clockwise
 # Set to -1 or -2 for anti-clockwise
 
 # Read wait time from command line
-WaitTime = 1 / float(1000)
+WaitTime = 10 / float(1000)
 
 # Initialise variables
 StepCounter = 0
